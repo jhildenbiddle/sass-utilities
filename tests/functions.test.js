5832b8a49327c8bffa901270638468eacca5768f
John Hildenbiddle <john@hildenbiddle.com>
2022-10-13 14:35:31 -0500

    Add tests

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -4,13 +4,34 @@ describe('chevron-data-uri', () => {
     test('Compile', () => {
         const result = compileSass(`
             .myclass {
-                background: red;
-
-                nested {
-                    color: blue;
-                }
+                background: chevron-data-uri(right);
             }
-        `, false, false);
+        `);
+
+        expect(result).toMatchSnapshot();
+    });
+});
+
+describe('svg-data-uri', () => {
+    test('Compile', () => {
+        const svg = '<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>';
+        const result = compileSass(`
+            .myclass {
+                background: svg-data-uri('${svg}');
+            }
+        `);
+
+        expect(result).toMatchSnapshot();
+    });
+});
+
+describe('_triangle-data-uri', () => {
+    test('Compile', () => {
+        const result = compileSass(`
+            .myclass {
+                background: triangle-data-uri(right);
+            }
+        `);
 
         expect(result).toMatchSnapshot();
     });
--- tests/helpers/compile-sass.js
+++ tests/helpers/compile-sass.js
@@ -1,39 +1,26 @@
-const fs = require('fs');
-const path = require('path');
 const sass = require('sass');
 
-function compileSass(sassText, autoImport = true, updatePaths = true) {
-    const testFilePath = path.resolve(process.cwd(), 'tests',  'test.scss')
+function dedent(str) {
+    str = str.replace(/^ *\n+/, '');
+    let match = str.match(/^\s+/);
+    return match ? str.replace(new RegExp('^'+match[0], 'gm'), '') : str;
+}
+
+function compileSass(sassText, autoImport = true) {
+    sassText = dedent(sassText);
 
     if (autoImport) {
         sassText = `@use "${process.cwd()}/_index" as *;\n` + sassText;
     }
 
-    if (updatePaths) {
-        // Matches @ rules and injects current working director into path if not found
-        // Ex: @use "file"; => @use "Users/john/path/to/file";
-        const reURLs = new RegExp(`@([a-z]*) ['"](?!${process.cwd()})([^'"]*)['"]`, 'g');
+    // NOTE: Using legacy Sass API to work around Jest compatibility issue
+    // 1. https://github.com/sass/dart-sass/issues/710
+    // 2. https://github.com/sass/dart-sass/issues/1692
+    const result = sass.renderSync({
+        data: sassText
+    });
 
-        sassText = sassText.replace(reURLs, `@$1 "${process.cwd()}/$2"`.replace(/\/\//, '/'));
-    }
-
-    sassText = sassText.trim();
-
-    console.log({ sassText, testFilePath });
-
-    // TODO: As of Sass v1.54.0, the compileString() method is unable import
-    // stylesheets using rules like @use and @forward. The following line should
-    // be all we need but it currently does not work as expected. return
-    // const result = sass.compileString(sassText);
-
-    // The following read/write operation is a hack to address the issue
-    // described above. The workaround is to write contents to a file and
-    // process it using the compile() method instead of using compileString().
-    fs.writeFileSync(testFilePath, sassText);
-
-    const result = sass.compile(testFilePath);
-
-    return result.css;
+    return result.css.toString();
 }
 
 module.exports = compileSass;
